488c6cd737b09cd961c8677559e1398a6766fb8a
Justin Ng <justin@MacBook-Pro-2.local>
2017-11-03 14:32:29 +0000

    task2 edit

--- task2.c
+++ task2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+//#include <stdbool.h>
+
 
 int calcPrime(int a, int b);
 int calcPow(int a, int b);
@@ -15,7 +17,7 @@ int main(int argc, char **argv)
 
 	int inputPrimeOne = strtol(argv[1], NULL, 10), inputPrimeTwo = strtol(argv[2], NULL, 10);
 
-	if (inputPrimeOne >= 1 && inputPrimeTwo <= 1000000 && argc == 3)
+	if (inputPrimeOne >= 2 && inputPrimeTwo <= 1000000 && argc == 3)
 	{
 		//for (i = inputPrimeOne; i <= inputPrimeTwo; i++)
 		//{
@@ -31,6 +33,34 @@ int main(int argc, char **argv)
 int calcPrime(int a, int b)
 {
 	int i, isPrime;
+
+	int isPrime(int a)
+	{
+		double aa = a;
+		if (a == 1)
+		{
+			return 0;
+		}
+	    if (a == 2)
+	    {
+	    	return 1;
+	    }
+	    if (a % 2 == 0)
+	    {
+	    	return 0;
+	    }
+	    int boundary = floor(sqrt(aa));
+
+	    for (int i = 3; i <= boundary; i += 2)
+	    {
+	        if (a % i == 0)
+	        {
+	        	return 0;
+	        }
+	    }
+
+    	return 1;
+	}
 
 	for (i = a; i <= b; i++)
 	{
@@ -64,7 +94,7 @@ int calcPrime(int a, int b)
 			isPrime = 0;
 		}*/
 
-		if (isPrime == 1)
+		if (isPrime(i) == 1)
 		{
 			printf("%d\n", i);
 		}
