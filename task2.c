de5bb4159bb044061645c24cb4cb6502d1bbc574
Justin Ng <justin@MacBook-Pro-2.local>
2017-11-04 17:59:27 +0000

    task2 edit

--- task2.c
+++ task2.c
@@ -5,7 +5,6 @@
 
 
 int calcPrime(int a, int b);
-int calcPow(int a, int b);
 
 int main(int argc, char **argv)
 {
@@ -21,46 +20,18 @@ int main(int argc, char **argv)
 	{
 		//for (i = inputPrimeOne; i <= inputPrimeTwo; i++)
 		//{
-			printf("%d", calcPrime(inputPrimeOne, inputPrimeTwo));
+			printf("%d\n", calcPrime(inputPrimeOne, inputPrimeTwo));
 		//}
 	}
 	else
 	{
-		printf("Error: Enter two positive values between 1 and 1000000\n");
+		printf("Error: Enter two positive values between 2 and 1000000\n");
 		return 1;
 	}
 }
 int calcPrime(int a, int b)
 {
 	int i, j, isPrime;
-
-	/*int isPrime(int a)
-	{
-		double aa = a;
-		if (a == 1)
-		{
-			return 0;
-		}
-	    if (a == 2)
-	    {
-	    	return 1;
-	    }
-	    if (a % 2 == 0)
-	    {
-	    	return 0;
-	    }
-	    int boundary = floor(sqrt(aa));
-
-	    for (int i = 3; i <= boundary; i += 2)
-	    {
-	        if (a % i == 0)
-	        {
-	        	return 0;
-	        }
-	    }
-
-    	return 1;
-	}*/
 
 	for (i = a; i <= b; i++)
 	{
@@ -78,52 +49,5 @@ int calcPrime(int a, int b)
 			printf("%d\n", i);
 		}
 	}
-
-	for (i = a; i <= b; i++)
-	{
-		/*if (i <= 1)
-		{
-			isPrime = 0;
-		}
-		if (i <= 3)
-		{
-			isPrime = 1;
-		}
-		if (i % 2 == 0 || i % 3 == 0)
-		{
-			isPrime = 0;
-		}
-		for (int j = 5; j * j <= i; j = j+6)
-		{
-			if (i % j == 0 || i % (j+2) == 0)
-			{
-				isPrime = 0;
-			}
-		}*/
-		//isPrime = 1;
-
-		/*if ((calcPow(5, i - 1)) % i == 1)
-		{
-			isPrime = 1;
-		}
-		else
-		{
-			isPrime = 0;
-		}
-
-		if (i == 1)
-		{
-			printf("%d\n", i);
-		}*/
-	}
-}
-int calcPow(int a, int b)
-{
-	int i, num = 1;
-
-	for (i = 0; i < b; i++)
-	{
-		num *= a;
-	}
-	return num;
+	return 0;
 }
