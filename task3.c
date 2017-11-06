ee099af020f595c2b4811c2fb4d2b281eede9af7
Justin Ng <justin@MacBook-Pro-2.local>
2017-11-06 20:56:44 +0000

    edit & upload

--- task3.c
+++ task3.c
@@ -3,5 +3,28 @@
 
 int main(int argc, char **argv)
 {
+	if (argc == 1 || argc == 2 || argc == 3)
+	{
+		printf("Error: Enter three parameters\n");
+		return 1;
+	}
 
+	char *nd;
+	int inputCell = strtol(argv[1], &nd, 10), inputGen = strtol(argv[2], &nd, 10), inputRuleSet = strtol(argv[3], &nd, 10);
+
+	if (*nd != '\0')
+	{
+		printf("Error: Enter three parameters\n");
+		return 1;
+	}
+	if (inputCell >= 0 && inputGen <= 0 && inputRuleSet >= 0 && inputRuleSet <= 255 && argc == 4)
+	{
+		printf("Acceptable\n");
+		return 0;
+	}
+	else
+	{
+		printf("Error: Enter three parameters\n");
+		return 1;
+	}
 }
